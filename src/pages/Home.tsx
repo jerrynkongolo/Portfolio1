549f17fd7e19bad8b93df0c24334e725a25edd2c
jerrynkongolo <jerrynkongolo@icloud.com>
2024-11-14 02:32:50 +0200

    My name changes from jerryyyy to Jerry

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
       <h1 className="font-display text-4xl md:text-6xl font-bold mb-4 text-gray-900 dark:text-white/90
                    bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-gray-600 dark:from-white dark:to-white/70
                    animate-gradient bg-300% tracking-tight">
-        Hi, I'm Jerryyyy from Raffiki Studio
+        Hi, I'm Jerry from Raffiki Studio
       </h1>
       
       <p className="font-sans text-lg md:text-xl mb-8 text-gray-600 dark:text-white/70
