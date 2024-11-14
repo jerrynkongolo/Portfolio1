5bed7bb8749e6d7e6bd7a2c2cd9725672f9d739a
jerrynkongolo <jerrynkongolo@icloud.com>
2024-11-14 02:43:02 +0200

    made changes to the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ function About() {
       <Navigation />
       
       <div className="max-w-[90%] mx-auto pt-36">
-        <div className="max-w-3xl">
+        <div className="max-w-2xl">
           <h1 className="font-display text-8xl font-bold mb-12 text-gray-900 dark:text-white/90 tracking-tight">
             ABOUT
           </h1>
@@ -17,7 +17,7 @@ function About() {
             <div className="absolute left-0 right-[30%] h-px bg-gray-900 dark:bg-white/90 mb-12" />
           </div>
           
-          <div className="mt-24 space-y-8 max-w-2xl">
+          <div className="mt-24 space-y-8">
             <p className="text-xl text-gray-800 dark:text-white/80 leading-relaxed">
               Hey, I'm Jerry Nkongolo, known as Sharlee on social media. I'm a multidisciplinary creative combining photography, front-end development, UI/UX design, and traditional painting. Passionate about pop music, I love creating portraits and universes inspired by what I listen to, and I'm always curious to learn more about new technologies and creative coding.
             </p>
